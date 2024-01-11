ce4217b684118be45547fa6de69475584960750f
Priscilla-22 <priscillamw123@gmail.com>
2024-01-11 19:21:34 +0300

    style: label the facts gving them unique ids'

--- cats.js
+++ cats.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     'https://images.unsplash.com/photo-1589883661923-6476cb0ae9f2?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2F0c3xlbnwwfHwwfHx8MA%3D%3D',
     'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNhdHN8ZW58MHx8MHx8fDA%3D',
   ];
-    
+
   // Fetch dog facts from the API
   fetch('https://cat-fact.herokuapp.com/facts')
     .then((response) => response.json())
@@ -21,22 +21,28 @@ document.addEventListener('DOMContentLoaded', function () {
         imgElement.src = exampleImageUrl;
         imgElement.alt = 'Dog';
 
-        const factElement = document.createElement('p');
-        factElement.textContent = dog.text;
-
         const imgDiv = document.createElement('div');
         imgDiv.classList.add('image-container');
         imgDiv.appendChild(imgElement);
 
-        const factDiv = document.createElement('div');
-        factDiv.classList.add('fact-container');
-        factDiv.appendChild(factElement);
-
         const cardContainer = document.createElement('div');
         cardContainer.classList.add('card');
         cardContainer.appendChild(imgDiv);
-        cardContainer.appendChild(factDiv);
 
+        // Create and append h2 elements for each fact
+        for (let i = 1; i <= 1; i++) {
+          const factNum = document.createElement('h2');
+          factNum.classList.add('fact-number');
+          factNum.textContent = `Fact ${index+1}`;
+          cardContainer.appendChild(factNum);
+        }
+
+        // Create and append the paragraph with dog facts
+        const factElement = document.createElement('p');
+        factElement.textContent = dog.text;
+        cardContainer.appendChild(factElement);
+
+        // Append the card container to the dog container
         dogContainer.appendChild(cardContainer);
       });
     })
